2b6ec609a11c35384e8ec06a1dd008df54a58f05
ayoubbellaari@gmail.com <ayoubbellaari@gmail.com>
2023-04-12 15:58:38 +0000

    fourth commit

--- ajaxData.php
+++ ajaxData.php
@@ -1,8 +1,8 @@
 <?php
-$client = true;
-
-    include_once("connexion.php");
-    $pdo = new connect();
+// $client = true;
+include_once("connexion.php");
+$pdo = new connect();
+// include_once("main.php");
 
     // $conn = mysqli_connect("localhost","root","","gestion_commande");
 
@@ -20,49 +20,49 @@ foreach($pdostmt1->fetchAll(PDO::FETCH_ASSOC) as $tabvalues) {
 $pdostmt1->closeCursor();
 
 
-// if(!empty($_POST["depart_code"])){
-//     $query = "SELECT * from villes_france where ville_departement=:code_departement order by ville_nom asc";
-//     $pdostmt = $pdo->prepare($query);
-//     $pdostmt->execute(["code_departement"=>$_POST["depart_code"]]);
+if(!empty($_POST["depart_code"])){
+    $query = "SELECT * from villes_france where ville_departement=:code_departement order by ville_nom asc";
+    $pdostmt = $pdo->prepare($query);
+    $pdostmt->execute(["code_departement"=>$_POST["depart_code"]]);
 
-//     while($row = $pdostmt->fetch(PDO::FETCH_ASSOC)):
-//         echo "<option value=".$row["ville_id"].">".$row["ville_nom"]."</option>";
-//     endwhile;
+    while($row = $pdostmt->fetch(PDO::FETCH_ASSOC)):
+        echo "<option value=".$row["ville_id"].">".$row["ville_nom"]."</option>";
+    endwhile;
 
-//     $pdostmt->closeCursor();
-// }
+    $pdostmt->closeCursor();
+}
 
-// echo json_encode($_POST);
+echo json_encode($_POST);
 
 
-// if(!empty($_POST["inputnom"]) && !empty($_POST["inputville"]) && !empty($_POST["inputtel"])){
-//     $query = "insert into client(nom,ville_id,telephone) values (:nom,:ville,:tel)";
-//     $pdostmt = $pdo->prepare($query);
-//     $result = $pdostmt->execute(["nom"=>$_POST["inputnom"],"ville"=>$_POST["inputville"],"tel"=>$_POST["inputtel"]]);
+if(!empty($_POST["inputnom"]) && !empty($_POST["inputville"]) && !empty($_POST["inputtel"])){
+    $query = "insert into client(nom,ville_id,telephone) values (:nom,:ville,:tel)";
+    $pdostmt = $pdo->prepare($query);
+    $result = $pdostmt->execute(["nom"=>$_POST["inputnom"],"ville"=>$_POST["inputville"],"tel"=>$_POST["inputtel"]]);
 
-//     if($result){
-//         $response=[
-//             "value"=>true,
-//             "msg"=>"Ajout avec succes"
-//         ];
-//     }else{
-//         $response=[
-//             "value"=>false,
-//             "msg"=>$pdostmt->errorInfo()
-//         ];
-//     }
+    if($result){
+        $response=[
+            "value"=>true,
+            "msg"=>"Ajout avec succes"
+        ];
+    }else{
+        $response=[
+            "value"=>false,
+            "msg"=>$pdostmt->errorInfo()
+        ];
+    }
 
-//     echo json_encode($response);
-//     $pdostmt->closeCursor();
-// }
+    echo json_encode($response);
+    $pdostmt->closeCursor();
+}
+
 
 if(!empty($_GET["client"])){
 
     $query = "select * from client as c,villes_france as v,departement as d
-         where c.ville_id=v.ville_id and v.ville_departement=d.departement_code";
+    where c.ville_id=v.ville_id and v.ville_departement=d.departement_code";
     $pdostmt = $pdo->prepare($query);
     $pdostmt->execute();
-
 
     while($ligne = $pdostmt->fetch(PDO::FETCH_ASSOC)): 
         $count++;
@@ -74,7 +74,7 @@ if(!empty($_GET["client"])){
         <td><?php echo $ligne["ville_nom"]; ?></td>
         <td><?php echo $ligne["telephone"]; ?></td>
         <td>
-            <a href="modifClient.php?id=<?php echo $ligne["idclient"] ?>" class="btn btn-success">
+            <a onclick=update_client(<?php echo $ligne["idclient"]?>) class="btn btn-success">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                 <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
             </svg>
@@ -107,9 +107,37 @@ if(!empty($_GET["client"])){
     </div>
     </div>
 
-    <?php endwhile;
-    } 
-    ?>
+    <?php endwhile; 
+    $pdostmt->closeCursor();
+
+}
+
+if(!empty($_GET["updated_client"])){
+    $query = "select * from client where idclient=:id";
+    $pdostmt= $pdo->prepare($query);
+    $pdostmt->execute(["id"=>$_GET["updated_client"]]);
+    $result = $pdostmt->fetch(PDO::FETCH_ASSOC);
+
+    if($result)
+    {
+        $response = [
+            "status" => true,
+            "data" => $result
+        ];
+    }else{
+        $response = [
+            "status" => false,
+            "data" => $pdostmt->errorInfo()
+        ];
+    }
+    
+    echo json_encode($response);
+
+}
+
+
+
+?>
 
 
 
--- client.php
+++ client.php
@@ -16,14 +16,12 @@ foreach($pdostmt1->fetchAll(PDO::FETCH_ASSOC) as $tabvalues) {
 }
 $pdostmt1->closeCursor();
 
-
 if(!empty($_POST["inputnom"]) && !empty($_POST["inputville"]) && !empty($_POST["inputtel"])){
     $query1 = "insert into client(nom,ville,telephone) values (:nom,:ville,:tel)";
     $pdostmt1 = $pdo->prepare($query1);
     $pdostmt1->execute(["nom"=>$_POST["inputnom"],"ville"=>$_POST["inputville"],"tel"=>$_POST["inputtel"]]);
     $pdostmt1->closeCursor();
-    header("Location:client.php");
-        
+    header("Location:client.php");     
 }
 
 $query2 = "SELECT * from departement order by departement_nom asc";
@@ -32,7 +30,22 @@ $pdostmt2->execute();
 
 ?>
 
-<script>
+<script >
+    function update_client(id){
+        // console.log(id);
+        $.ajax({
+            type: "GET",
+            url: "ajaxData.php?updated_client="+id,
+            dataType: "json",
+            success: function(response){
+                console.log(response);
+                // $("#client_id").val(response.data.idclient);
+                // $("inputnom").val(response.data.nom);
+                // $("inputtel").val(response.data.telephone);
+                $("#clientModal").modal("show");
+            }
+        })
+    }
 
   $(document).ready(function(){
     $("#inputdepart").on("change",function(){
@@ -70,7 +83,6 @@ $pdostmt2->execute();
                 $("#clientModal").modal("hide");
                 $("#clientform")[0].reset();
                 $("#inputville").val('');
-
             }else{
                 toastr.error(response.msg);
             }
@@ -81,18 +93,20 @@ $pdostmt2->execute();
         });
     })
 
-    function getClients(){
-        $.ajax({
-            type: "GET",
-            url: "ajaxData.php?client=true",
-            dataType: "html",
-            succes: function(response){
-                console.log(response);
-            }
-        })
-    };
+    // function getClients(){
+    //     $.ajax({
+    //         type: "GET",
+    //         url: "ajaxData.php?client=true",
+    //         dataType: "html",
+    //         succes: function(response){
+    //             console.log(response);
+    //             $("#clients").html(response);
+    //             // $('#myTable').DataTable();
+    //         }
+    //     })
+    // };
 
-    getClients();
+    // getClients();
 
   })
 </script>
@@ -150,9 +164,9 @@ $pdostmt2->execute();
                     </div>
 
             </div>
-        <div class="modal-footer">
-                <button type="submit" class="btn btn-primary">Ajouter</button>
-        </div>
+            <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Ajouter</button>
+            </div>
         </form>
         </div>
     </div>
@@ -166,7 +180,11 @@ $pdostmt2->execute();
     </a> -->
 
     <?php
-         
+            $query = "select * from client as c,villes_france as v,departement as d
+            where c.ville_id=v.ville_id and v.ville_departement=d.departement_code";
+            $pdostmt = $pdo->prepare($query);
+            $pdostmt->execute();
+        
 
     ?>
 
@@ -181,7 +199,51 @@ $pdostmt2->execute();
                 <th>Action</th>
             </tr>
         </thead>
-        <tbody>
+        <tbody id="clients">
+     <?php   while($ligne = $pdostmt->fetch(PDO::FETCH_ASSOC)): 
+        $count++;
+    ?>
+    <tr>
+        <td><?php echo $ligne["idclient"]; ?></td>
+        <td><?php echo $ligne["nom"]; ?></td>
+        <td><?php echo $ligne["departement_nom"]; ?></td>
+        <td><?php echo $ligne["ville_nom"]; ?></td>
+        <td><?php echo $ligne["telephone"]; ?></td>
+        <td>
+            <a onclick=update_client(<?php echo $ligne["idclient"]?>) class="btn btn-success">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+            </svg>
+            </a>
+
+            <button type="button"class="btn btn-danger" <?php if(in_array($ligne["idclient"],$list)) {echo "disabled";} ?> data-bs-toggle="modal" data-bs-target="#deleteModal<?php echo $count ?>">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+            </svg>
+            </button>
+        </td>
+    </tr>
+
+    <!-- Modal -->
+    <div class="modal fade" id="deleteModal<?php echo $count ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Suppression</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            Voulez vous vraiment supprimer ce client ?
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuller</button>
+            <a href="delete.php?idClt=<?php echo $ligne["idclient"] ?>" class="btn btn-danger">Supprimer</a>
+        </div>
+        </div>
+    </div>
+    </div>
+
+    <?php endwhile; ?>
 
         </tbody>
     </table>
